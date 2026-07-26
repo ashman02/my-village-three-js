289493eaf53382dba0439d5683859499e00a5517
cforChobar <sidhuashman02@gmail.com>
2026-07-26 16:13:15 +0530

    Experience.js - Instantiated world inside our experience class.
    Created world folder. All the visual stuff will live here.
    
    World.js - Add World class to manage scene elements in Experience

---
+++ src/Experience/World/World.js
@@ -0,0 +1,9 @@
+
+// All the visible stuff will be here inside and Wrold folder and we will instantiate that stuff here inside World class.
+
+
+export default class World {
+    constructor(){
+        
+    }
+}
